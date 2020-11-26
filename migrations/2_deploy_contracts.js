9fb76011665e8d54d790bce6e90d648a7d7af480
SEFI2 <kadirpili@gmail.com>
2020-11-27 01:29:30 +0300

    Delete old tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
-const TadaamCoin = artifacts.require("TadaamCoin");
+const RunCode = artifacts.require("RunCode");
 
 module.exports = async (deployer) => {
-  await deployer.deploy(ConvertLib);
-  await deployer.link(ConvertLib, MetaCoin);
-  await deployer.deploy(MetaCoin);
-  await deployer.deploy(APIConsumer);  
-  await deployer.deploy(TadaamCoin, []);
+  await deployer.deploy(RunCode, null);
 };

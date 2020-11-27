0f7b3ac44b63ab25132fc57dc49daf9a7dbccf7f
SEFI2 <kadirpili@gmail.com>
2020-11-27 16:38:10 +0300

    Refactor code

--- contracts/RunCode.sol
+++ contracts/RunCode.sol
@@ -19,72 +19,63 @@ contract RunCode {
     }
 
     // Events
-    event RunCodeForKey(string codeKey);
-    
+    event ReceivedStatus(string codeKey, string taskId, CodeStatus status);
+
     // Variables
-    mapping (string => CodeStatus) statuses;
-    mapping (address => int256) balances;
-    mapping (Task => address) taskToAddress;
-    mapping (string => Task) tasks;
-    mapping (string => address) codeToUser;
+    mapping (string => CodeStatus) internal statuses;
+    mapping (address => int256) internal balances;
+    mapping (string => Task) internal tasks;
+    mapping (string => address payable) internal codeToUser;
 
     // Modifiers
     modifier ifOracle {
-        require(msg.sender == oracle);
+        require(msg.sender == oracle, "Sender is not oracle");
+        _;
     }
 
-    modifier ifTaskOwner (string taskId) {
-        require(msg.sender == tasks[taskId].owner);
+    modifier ifTaskOwner (string memory taskId) {
+        require(msg.sender == tasks[taskId].owner, "Sender is not owner");
+        _;
     }
     
     // Constructor
-    constructor (address _oracle) {
+    constructor (address _oracle) public {
         oracle = _oracle;
     }
 
     // Functions 
-    function registerTask(string taskId) {
-        Task task = Task({ owner: msg.address, balance: msg.value });
+    function registerTask(string memory taskId, uint256 reward) public payable {
+        Task memory task = Task({ id: taskId, owner: msg.sender, balance: msg.value, reward: reward });
         tasks[taskId] = task;
     }
 
-    function withdrawAll(string memory taskId) ifTaskOwner(taskId) {
-        Task task = tasks[taskId];
+    function withdrawAll(string memory taskId) public ifTaskOwner(taskId) {
+        Task memory task = tasks[taskId];
         msg.sender.transfer(task.balance);
         delete tasks[taskId];
     }
 
-    function withdraw(string memory taskId, uint256 value) ifTaskOwner(taskId) {
-        Task task = tasks[taskId];
-        require(task.balance >= value);
+    function withdraw(string memory taskId, uint256 value) public ifTaskOwner(taskId) {
+        Task memory task = tasks[taskId];
+        require(task.balance >= value, "Balance is less than value");
         msg.sender.transfer(value);
         task.balance -= value;
     }
 
-    function addSubmission(string memory codeKey) {
+    function addSubmission(string memory taskId, string memory codeKey) public {
 
     }
 
-    function sendStatus(string memory codeKey, string memory taskId, CodeStatus status) ifOracle returns(bool) {
-        if (status == SUCCESS) {
-            Task task = tasks[taskId];
-            require(task.balance >= task.reward, "Balance is less than reward amount");
-            address user = codeToUser[codeKey];
+    function sendStatus(string memory codeKey, string memory taskId, CodeStatus status) public ifOracle returns(bool) {
+        if (status == CodeStatus.ACCEPTED) {
+            Task memory task = tasks[taskId];
+            require(task.balance >= task.reward, "Balance is less than reward");
+            address payable user = codeToUser[codeKey];
             user.transfer(task.reward);
         } else {
             delete codeToUser[codeKey];
         }
-        emit receivedStatus(codeKey, taskId, status);
-        return true;
-    }
-
-    function RunCode(string codeKey) {
-        statuses[codeKey] = CodeStatus.PENDING;
-        emit RunCodeForKey(codeKey);
-    }
-
-    function CheckStatus(string codeKey) returns(bool) {
-
+        emit ReceivedStatus(codeKey, taskId, status);
         return true;
     }
 }
